b3544017a7470f117d59ce322469f067557e3527
Thomas Wozniakowski <thomas.wozniakowski@masabi.com>
2018-04-12 18:29:47 +0100

    task 25

--- src/iii_conventions/MyDate.kt
+++ src/iii_conventions/MyDate.kt
@@ -1,6 +1,17 @@
 package iii_conventions
 
-data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int)
+data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int) {
+
+    operator fun compareTo(other: MyDate): Int {
+        if (year != other.year) {
+            return Integer.compare(year, other.year)
+        } else if (month != other.month) {
+            return Integer.compare(month, other.month)
+        } else {
+            return Integer.compare(dayOfMonth, other.dayOfMonth)
+        }
+    }
+}
 
 operator fun MyDate.rangeTo(other: MyDate): DateRange = todoTask27()
 
